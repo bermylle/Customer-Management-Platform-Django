d0213a80b1dd5acf4589a15b961a8ad281781731
bermylle <50631121+bermylle@users.noreply.github.com>
2020-05-02 20:58:03 +0800

    password reset e-mail update

--- cmp/accounts/views.py
+++ cmp/accounts/views.py
@@ -45,13 +45,6 @@ def registerPage(request):
 			user = form.save()
 			username = form.cleaned_data.get('username')
 
-			group = Group.objects.get(name = 'customer')
-			user.groups.add(group)
-
-			Customer.objects.create (
-				user=user,
-				name=user.username,
-				)
 			messages.success(request, 'Account created for ' + username)
 
 			return redirect('login')
